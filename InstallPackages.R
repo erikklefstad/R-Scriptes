3b2f0555555b2ffe8e374788c14abcaaad3eb81c
erikklefstad <47761486+erikklefstad@users.noreply.github.com>
2019-03-02 17:00:44 -0600

    Update InstallPackages.R

--- InstallPackages.R
+++ InstallPackages.R
@@ -1,8 +1,13 @@
 # Create a file of the packages in current instance of R and write
+# This is done on the local/current machine
 pkgs <- installed.packages()
 write.table(pkgs, file = "CurrentPackages.csv")
 
 # Read in dataframe of packages and install
+# This is done on the machine where the packages need to be installed
+
+pkgfile <- "CurrentPackages.csv"
+
 pkgs <- read.table(pkgfile, header = TRUE)
 
 libs <- library()$results[, 1]
